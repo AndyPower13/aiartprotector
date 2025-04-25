76102a7a54b8697d3225f7403272c4fa86bca180
AndyPower13 <a.rozlutskyi1@gmail.com>
2025-04-24 23:31:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -53,5 +53,4 @@ def set_webhook():
     requests.post(url, json=data)
 
 if __name__ == "__main__":
-    set_webhook()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
